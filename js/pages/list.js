4296c468c828b53a22fcccee0cd2ce730d280723
angelolustosa <angelolustosa@gmil.com>
2023-05-09 21:25:48 -0300

    Consumindo API de listar carros

--- js/pages/list.js
+++ js/pages/list.js
@@ -1,3 +1,32 @@
 function list() {
-    return "<ul><li>Vitor 1</li> <li>Vitor 2</li> </ul>";
+    fetch('http://localhost:8000/carros')
+        .then(res => res.json())
+        .then(dados => {
+            dados.map(cada => {
+                table_cars.innerHTML += `
+                    <tr>
+                        <td>${cada.id}</td>
+                        <td>${cada.marca}</td>
+                        <td>${cada.modelo}</td>
+                    </tr>    
+                `; 
+            })
+        });
+
+    // npx json-server --port 8000 --watch db.json     
+    return `
+        <table class="table table-hover table-striped">
+            <thead class="table-dark">
+                <tr>
+                    <th>#ID</th>
+                    <th>Marca</th>
+                    <th>Modelo</th>
+                    <th>Placa</th>
+                    <th>Foto</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody id="table_cars"></tbody>
+        </table>
+    `;
 }
